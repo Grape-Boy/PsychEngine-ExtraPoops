353afaef9aa670c5a05439994a91633cf67a3cde
Grape-Boy <cherryflosstheskeleton@gmail.com>
2022-06-13 15:12:54 +0200

    fix html5 pull request thingy but unrelated commit history

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2477,9 +2477,11 @@ class FunkinLua {
 	}*/
 
 	function getErrorMessage() {
+		#if LUA_ALLOWED
 		var v:String = Lua.tostring(lua, -1);
 		Lua.pop(lua, 1);
 		return v;
+		#end
 	}
 
 	public function call(func:String, args:Array<Dynamic>): Dynamic{
